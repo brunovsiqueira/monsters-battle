2719c9cc1b0c307b1052715a903de4f57dbd7829
Bruno Siqueira <brunovsiq@gmail.com>
2023-04-29 21:05:45 -0300

    remove deprecated

--- lib/utils/player_type_enum.dart
+++ lib/utils/player_type_enum.dart
@@ -1,10 +1,6 @@
-import 'package:flutter/foundation.dart' show describeEnum;
-
 enum PlayerType { player, computer }
 
 extension PlayerTypeExtension on PlayerType {
-  String get name => describeEnum(this);
-
   String get playerName {
     switch (this) {
       case PlayerType.player:
