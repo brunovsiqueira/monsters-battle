c603e09d5cea094f7ae8fd067b099cd0b6e9a619
Bruno Siqueira <brunovsiq@gmail.com>
2023-05-03 23:33:12 -0300

    create MonstersService abstract class

---
+++ lib/services/monsters_service.dart
@@ -0,0 +1,8 @@
+import 'dart:async';
+import 'package:dartz/dartz.dart';
+import 'package:monsters_battle/errors/base_failures.dart';
+import 'package:monsters_battle/models/monster.dart';
+
+abstract class MonstersService {
+  Future<Either<Failure, List<MonsterModel>>> getMonsters();
+}
